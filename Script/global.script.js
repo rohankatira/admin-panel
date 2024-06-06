c537991ee289a8523f5b3627243d3e25b2c713f7
rohankatira <rohankatira1@gmail.com>
2024-06-07 05:27:08 +0530

    change

--- Script/global.script.js
+++ Script/global.script.js
@@ -1,7 +1,7 @@
-$("#icons,#google,#profile,#table,#login,#register,#logout").hide();
+$("#calender,#google,#profile,#table,#login,#register,#logout").hide();
 
 $(".dashbord>button").on("click",function(){
-    $("#Dashbord,#icons,#google,#profile,#table,#login,#register,#logout").hide();
+    $("#Dashbord,#calender,#google,#profile,#table,#login,#register,#logout").hide();
     $($(this).attr("for")).show();
     
 })
