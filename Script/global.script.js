064f37f2a2ebb92ca42130eb5d2b4771daf5f60f
rohankatira <rohankatira1@gmail.com>
2024-06-08 16:35:50 +0530

    change

--- Script/global.script.js
+++ Script/global.script.js
@@ -1,7 +1,7 @@
-$("#calender,#google,#profile,#table,#login,#register,#logout").hide();
+$("#calender-main,#google,#profile,#table,#login,#register,#logout").hide();
 
 $(".dashbord>button").on("click",function(){
-    $("#Dashbord,calender-main,#google,#profile,#table,#login,#register,#logout").hide();
+    $("#Dashbord,#calender-main,#google,#profile,#table,#login,#register,#logout").hide();
     $($(this).attr("for")).show();
     
 })
@@ -143,4 +143,27 @@ var options = {
      $("#slider").toggle();
   })
 
+  $(".bx-menu-alt-left").hide()
   
+  // ------------------- media - query ----------------
+
+  if(visualViewport.width <= 600){
+    $(".bx-menu").hide()
+    $(".bx-menu-alt-left").show()
+    $("#slider").hide()
+    $(".bx-menu-alt-left").on("click",function(){
+      $("#slider").css("width","200px");
+      $("#slider").css("position","absolute");
+      $("#slider").css("z-index","20");
+      $("#slider").toggle();
+   })
+
+  }
+
+
+
+
+  var calendarInstance1 = new calendarJs( "calendar", {
+    manualEditingEnabled: true
+    
+  } );
